973ae738681ecdce7c3f90cd6fa5da7416b8fe35
heesane <tees3359@gmail.com>
2023-10-23 14:15:32 +0000

    linux

--- data/mavericks/main.py
+++ data/mavericks/main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 from routers.port import port_router
 from routers.admin import admin_router
 from routers.robot import robot_router
+from starlette.middleware.cors import CORSMiddleware
+origins=[
+        "*"
+]
 app = FastAPI(prefix="v1/")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(port_router.router)
 app.include_router(admin_router.router)
